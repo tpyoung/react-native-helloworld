bc7dae9bb041bb48b229d2212a3211f5774b0416
tpyoung <tylerparkeyoung@gmail.com>
2017-01-05 18:16:14 -0800

    Enables gps location searching

--- SearchPage.js
+++ SearchPage.js
@@ -123,6 +123,21 @@ class SearchPage extends Component {
   	this._executeQuery(query);
 	}		
 
+	onLocationPressed() {
+		navigator.geolocation.getCurrentPosition(
+			location => {
+				var search = location.coords.latitude + ',' + location.coords.longitude;
+				this.setState({searchString: search});
+				var query = urlForQueryAndPage('centre_point', search, 1);
+				this._executeQuery(query);
+			},
+			error => {
+				this.setState({
+						message: 'There was a problem with obtaining your location:' + error
+				});
+			}
+		);
+	}
 
 	render() {
 		var spinner = this.state.isLoading ?
@@ -156,8 +171,9 @@ class SearchPage extends Component {
 				  </TouchableHighlight>
 				</View>
 				<View style={styles.flowRight}>
-					<TouchableHighlight style={styles.button}
-					    underlayColor='#99d9f4'>
+					<TouchableHighlight style={styles.button} 
+						underlayColor='#99d9f4' 
+						onPress={this.onLocationPressed.bind(this)}>
 					  <Text style={styles.buttonText}>
 					  	Location
 					  </Text>
